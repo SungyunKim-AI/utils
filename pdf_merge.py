0cacd0ffeb967d4ee36074a0e76526e3ad2ae2b8
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-10-17 23:01:49 +0900

    Update pdf_merge.py

--- pdf_merge.py
+++ pdf_merge.py
@@ -2,16 +2,17 @@ import os
 from glob import glob
 from PyPDF2 import PdfFileMerger
 
-folder_path = 'mybook'      # Merge 할 PDF 폴더 경로
+folder_path = 'C:/Users/sungy/Downloads/ML_LectureNote'      # Merge 할 PDF 폴더 경로
 save_name = ''              # 저장 할 PDF 이름
 
 merger = PdfFileMerger()
 
-for f in glob(folder_path + '*.pdf'):
+for f in glob(folder_path + '/*.pdf'):
+    print(f)
     merger.append(f)
 
 if save_name != '':
     merger.write(os.path.join(folder_path, save_name)+ ".pdf")
 else:
-    merger.write(os.path.basename(folder_path) + ".pdf")
+    merger.write(folder_path + ".pdf")
 merger.close()
